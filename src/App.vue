96c9ff5cfe50bc9cefb47f7aa79a7488881bc69b
KARTHIK DILLIRAJ <kdilliraj@elastalink.com>
2025-07-30 19:15:13 -0400

    Initial commit of project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a-button type="primary">Primary Button</a-button>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
